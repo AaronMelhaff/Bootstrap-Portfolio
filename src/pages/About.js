cfa0f995139d02b0179e5da37b049e0b508148d9
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-06-23 19:33:39 -0700

    Initial work on skills and education tabs.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { Route, NavLink } from 'react-router-dom';
 import { Fade, Container, Row, Col, Card } from 'react-bootstrap';
 import PortraitImage from './../assets/images/portrait.jpg';
+import Skills from './Skills';
+import Education from './Education';
+import SVGIcon from '../components/SVGIcon';
+import icons from '../utilities/icons';
 
 const NoMatch = (props) => {
   return (
     <Fade in appear={true}>
-      <Container>
+      <Container className='mt-2'>
         <Row>
           <Col>
             <Card>
@@ -14,13 +19,23 @@ const NoMatch = (props) => {
               <Card.Body className='justify-content-center'>
                 <Card.Img className='portrait' src={PortraitImage} alt=' ' />
                 <Card.Title className='text-center text-primary'>
-                  <h1>About Me</h1>
+                  <h1 className='display-4'>About Me</h1>
                 </Card.Title>
                 <Card.Text>
                   I am a recently graduated software developer seeking opportunities to learn new technologies and create
                   positive user experiences. During my time at school, I have learned different agile methods for boosting 
                   productivity and communication. 
                 </Card.Text>
+                <NavLink to={props.match.url + '/skills'} className='btn btn-tertiary d-inline-flex'>
+                  <SVGIcon className='button-icon mr-2' path={icons('skills')} />
+                  <h4 className='m-0'>Skills</h4>
+                </NavLink>
+                <NavLink to={props.match.url + '/education'} className='btn btn-tertiary d-inline-flex'>
+                  <SVGIcon className='button-icon mr-2' path={icons('education')} />
+                  <h4 className='m-0'>Education</h4>
+                </NavLink>
+                <Route path={props.match.url + '/education'} component={Education}/>
+                <Route path={props.match.url + '/skills'} component={Skills}/>
               </Card.Body>
             </Card>
           </Col>
