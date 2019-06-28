1f91b8b6a60d79798ec65dc3fe937284f92d9a4a
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-06-28 06:17:29 -0700

    Added file for tracking projects.

---
+++ src/utilities/projects.js
@@ -0,0 +1,72 @@
+import NoMatch from "../pages/NoMatch";
+
+const projects = [
+  {
+    title: 'Adventure Game',
+    url: '/adventure-game',
+    component: NoMatch,
+    tags: ['java']
+  },
+  {
+    title: 'Hunger Games',
+    url: '/hunger-games',
+    component: NoMatch,
+    tags: ['java', 'data-structures']
+  },
+  {
+    title: 'Web Crawler',
+    url: '/web-crawler',
+    component: NoMatch,
+    tags: ['java', 'http']
+  },
+  {
+    title: 'Chat Program',
+    url: '/chat-program',
+    component: NoMatch,
+    tags: ['java', 'sockets']
+  },
+  {
+    title: 'Assassin Program',
+    url: '/assassin-program',
+    component: NoMatch,
+    tags: ['java', 'data-structures']
+  },
+  {
+    title: 'Name Registry',
+    url: '/name-registry',
+    component: NoMatch,
+    tags: ['java', 'file i/o']
+  },
+  {
+    title: 'Note Taker',
+    url: '/note-taker',
+    component: NoMatch,
+    tags: ['java', 'design-patterns', 'java-fx']
+  },
+  {
+    title: 'Paint Program',
+    url: '/paint-program',
+    component: NoMatch,
+    tags: ['java', 'design-patterns', 'java-fx']
+  },
+  {
+    title: 'My Portfolio',
+    url: '/bootstrap-portfolio',
+    component: NoMatch,
+    tags: ['javascript', 'react', 'web-development', 'bootstrap', 'front-end', 'sass']
+  },
+  {
+    title: 'Gloomtility',
+    url: '/gloomtility',
+    component: NoMatch,
+    tags: ['javascript', 'java', 'restful', 'api', 'json', 'postgresql', 'full-stack', 'aws', 'react', 'redux', 'spring', 'spring-boot', 'sass']
+  },
+  {
+    title: 'Schedule-IT',
+    url: '/schedule-it',
+    component: NoMatch,
+    tags: ['javascript', 'jquery', 'sass', 'php', 'api', 'mysql', 'sql', 'full-stack', 'json', 'bootstrap']
+  }
+];
+
+export default projects;
