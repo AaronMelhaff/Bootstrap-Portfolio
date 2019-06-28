2dd97927d72608f62c5844598d092e9283581d9d
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-06-27 20:07:41 -0700

    Added route for pallete and manifest comment.

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import SiteNav from './components/SiteNav';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
+import Pallete from './pages/Pallete';
 import NoMatch from './pages/NoMatch';
 
 function Routes(props) {
@@ -15,10 +16,20 @@ function Routes(props) {
         <Redirect exact from='/' to='/about' />
         <Route path='/about' component={About} />
         <Route path='/portfolio' component={Portfolio} />
+        <Route path='/pallete' component={Pallete} />
         <Route component={NoMatch} />
       </Switch>
     </Router>
   );
 }
+
+
+/* -- Route Manifest
+  - /
+    - /about
+      - /skills
+      - /education
+    - /portfolio
+*/
 
 export default Routes
