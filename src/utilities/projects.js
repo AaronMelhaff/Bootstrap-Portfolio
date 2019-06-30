8051dfd7867b3007f5c4f9fdad91fb1c37c0ca76
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-06-30 13:24:04 -0700

    Removed Http tag

--- src/utilities/projects.js
+++ src/utilities/projects.js
@@ -3,77 +3,77 @@ import NoMatch from "../pages/NoMatch";
 const projects = [
   {
     title: 'Adventure Game',
-    url: '/adventure-game',
+    url: 'adventure-game',
     date: 'Spring 2017',
     component: NoMatch,
     tags: ['java']
   },
   {
     title: 'Hunger Games',
-    url: '/hunger-games',
+    url: 'hunger-games',
     date: 'Spring 2017',
     component: NoMatch,
     tags: ['java', 'data-structures']
   },
   {
     title: 'Web Crawler',
-    url: '/web-crawler',
+    url: 'web-crawler',
     date: 'Fall 2017',
     component: NoMatch,
-    tags: ['java', 'http']
+    tags: ['java']
   },
   {
     title: 'Chat Program',
-    url: '/chat-program',
+    url: 'chat-program',
     date: 'Fall 2017',
     component: NoMatch,
     tags: ['java', 'sockets']
   },
   {
     title: 'Assassin Program',
-    url: '/assassin-program',
+    url: 'assassin-program',
     date: 'Spring 2017',
     component: NoMatch,
     tags: ['java', 'data-structures']
   },
   {
     title: 'Name Registry',
-    url: '/name-registry',
+    url: 'name-registry',
     date: 'Spring 2017',
     component: NoMatch,
-    tags: ['java', 'file i/o']
+    tags: ['java', 'file io']
   },
   {
     title: 'Note Taker',
-    url: '/note-taker',
+    url: 'note-taker',
     date: 'Fall 2018',
     component: NoMatch,
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'Paint Program',
-    url: '/paint-program',
+    url: 'paint-program',
     date: 'Fall 2018',
     component: NoMatch,
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'My Portfolio',
-    url: '/bootstrap-portfolio',
+    url: 'bootstrap-portfolio',
     date: 'Spring 2019',
     component: NoMatch,
     tags: ['javascript', 'react', 'web-development', 'bootstrap', 'front-end', 'sass']
   },
   {
     title: 'Gloomtility',
-    url: '/gloomtility',
+    url: 'gloomtility',
     date: 'Spring 2019',
     component: NoMatch,
     tags: ['javascript', 'java','web-development', 'front-end', 'restful', 'api', 'json', 'postgresql', 'full-stack', 'aws', 'react', 'redux', 'spring', 'spring-boot', 'sass']
   },
   {
     title: 'Schedule-IT',
-    url: '/schedule-it',
+    url: 'schedule-it',
     date: 'Summer 2018',
     component: NoMatch,
     tags: ['javascript', 'jquery', 'web-development', 'front-end', 'sass', 'php', 'api', 'mysql', 'sql', 'full-stack', 'json', 'bootstrap']
