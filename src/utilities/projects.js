5e161de0be18b118540b6657240992d8498fb5fd
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-06-30 12:33:59 -0700

    Added date to projects.

--- src/utilities/projects.js
+++ src/utilities/projects.js
@@ -4,66 +4,77 @@ const projects = [
   {
     title: 'Adventure Game',
     url: '/adventure-game',
+    date: 'Spring 2017',
     component: NoMatch,
     tags: ['java']
   },
   {
     title: 'Hunger Games',
     url: '/hunger-games',
+    date: 'Spring 2017',
     component: NoMatch,
     tags: ['java', 'data-structures']
   },
   {
     title: 'Web Crawler',
     url: '/web-crawler',
+    date: 'Fall 2017',
     component: NoMatch,
     tags: ['java', 'http']
   },
   {
     title: 'Chat Program',
     url: '/chat-program',
+    date: 'Fall 2017',
     component: NoMatch,
     tags: ['java', 'sockets']
   },
   {
     title: 'Assassin Program',
     url: '/assassin-program',
+    date: 'Spring 2017',
     component: NoMatch,
     tags: ['java', 'data-structures']
   },
   {
     title: 'Name Registry',
     url: '/name-registry',
+    date: 'Spring 2017',
     component: NoMatch,
     tags: ['java', 'file i/o']
   },
   {
     title: 'Note Taker',
     url: '/note-taker',
+    date: 'Fall 2018',
     component: NoMatch,
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'Paint Program',
     url: '/paint-program',
+    date: 'Fall 2018',
     component: NoMatch,
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'My Portfolio',
     url: '/bootstrap-portfolio',
+    date: 'Spring 2019',
     component: NoMatch,
     tags: ['javascript', 'react', 'web-development', 'bootstrap', 'front-end', 'sass']
   },
   {
     title: 'Gloomtility',
     url: '/gloomtility',
+    date: 'Spring 2019',
     component: NoMatch,
     tags: ['javascript', 'java','web-development', 'front-end', 'restful', 'api', 'json', 'postgresql', 'full-stack', 'aws', 'react', 'redux', 'spring', 'spring-boot', 'sass']
   },
   {
     title: 'Schedule-IT',
     url: '/schedule-it',
+    date: 'Summer 2018',
     component: NoMatch,
     tags: ['javascript', 'jquery', 'web-development', 'front-end', 'sass', 'php', 'api', 'mysql', 'sql', 'full-stack', 'json', 'bootstrap']
   }
