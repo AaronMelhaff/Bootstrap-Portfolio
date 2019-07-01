4f3d96105000eeeccd5c70389276e73b0045cfc7
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-07-01 08:44:43 -0700

    Removing unneeded margin class and adding lead class

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,24 +16,24 @@ const About = (props) => {
         <Row>
           <Col>
             <Card className='border-0'>
-              <Card.Header className='bg-tertiary text-light'><h2 className='m-0'>Aaron Melhaff</h2></Card.Header>
+              <Card.Header className='bg-tertiary text-light'><h2>Aaron Melhaff</h2></Card.Header>
               <Card.Body className='justify-content-center'>
                 <Card.Img className='portrait' src={PortraitImage} alt=' ' />
                 <Card.Title className='text-center text-primary'>
                   <h1 className='display-4'>About Me</h1>
                 </Card.Title>
-                <Card.Text className='px-4'>
+                <Card.Text className='px-4 lead'>
                   I am a recently graduated software developer seeking opportunities to learn new technologies and create
                   positive user experiences. During my time at school, I have learned different agile methods for boosting 
                   productivity and communication. 
                 </Card.Text>
                 <Link to={props.match.url + '/skills'} className='btn btn-tertiary d-inline-flex'>
                   <SVGIcon className='button-icon mr-2' path={icons('skills')} />
-                  <h4 className='m-0'>Skills</h4>
+                  <h4>Skills</h4>
                 </Link>
                 <Link to={props.match.url + '/education'} className='btn btn-tertiary d-inline-flex'>
                   <SVGIcon className='button-icon mr-2' path={icons('education')} />
-                  <h4 className='m-0'>Education</h4>
+                  <h4>Education</h4>
                 </Link>
                 <Switch>
                   <Route path={props.match.url + '/education'} component={Education}/>
