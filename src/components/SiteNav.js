8c35be4d8c4678bcd10dceb890e8a96be99bea73
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-07-01 09:46:56 -0700

    Removing unused class on nav links and removing facebook link.

--- src/components/SiteNav.js
+++ src/components/SiteNav.js
@@ -12,24 +12,21 @@ export default function SiteNav(props) {
         <Navbar.Toggle aria-controls='site-nav'/>
         <Navbar.Collapse id='site-nav'>
             <Nav className='w-356px'>
-              <Link to='/' exact className='btn btn-primary d-block nav-btn'>
+              <Link to='/' exact className='btn btn-primary nav-btn'>
                   <span className='mr-1'>About</span>
                   <SVGIcon className='button-icon icon-sm' path={icons('about')}/>
               </Link>
-              <Link to='/portfolio' className='btn btn-primary d-block nav-btn'>
+              <Link to='/portfolio' className='btn btn-primary nav-btn'>
                   <span className='mr-1'>Portfolio</span>
                   <SVGIcon className='button-icon icon-sm' path={icons('portfolio')}/>
               </Link>
-              <Link to='/contact' className='btn btn-primary d-block nav-btn'>
+              <Link to='/contact' className='btn btn-primary nav-btn'>
                   <span className='mr-1'>Contact</span>
                   <SVGIcon className='button-icon icon-sm' path={icons('contact')}/>
               </Link>
             </Nav>
             <hr className='d-lg-none border'/>
             <div className='d-flex justify-content-center justify-content-lg-end w-100' rel='noopener noreferrer'>
-              {/* <a className='btn btn-primary' href='https://www.facebook.com/aaron.melhaff' target='_blank' rel='noopener noreferrer'>
-                <SVGIcon className='button-icon icon-sm' path={icons('facebook')}/>
-              </a> */}
 
               <a className='btn btn-primary' href='https://github.com/AaronMelhaff' target='_blank' rel='noopener noreferrer'>
                 <SVGIcon className='button-icon icon-sm' path={icons('github')}/>
