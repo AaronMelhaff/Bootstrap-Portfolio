2ab9c4a6c85775d6bc33083cd23838eeeca474f2
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-07-15 15:25:37 -0700

    Restructuring and Filling out project info.

--- src/utilities/projects.js
+++ src/utilities/projects.js
@@ -1,81 +1,104 @@
-import NoMatch from "../pages/NoMatch";
+
 
 const projects = [
   {
     title: 'Adventure Game',
-    url: 'adventure-game',
+    id: 'adventure-game',
     date: 'Spring 2017',
-    component: NoMatch,
+    images: [
+      {
+        src: 'https://raw.githubusercontent.com/AaronMelhaff/AdventureProgram/master/src/resources/screenshots/fighting-monsters.PNG',
+        caption: 'Fighting a monster in the game.'
+      },
+      {
+        src: 'https://raw.githubusercontent.com/AaronMelhaff/AdventureProgram/master/src/resources/screenshots/picking-a-character.PNG',
+        caption: 'Picking a character at the start of the game.'
+      },
+      {
+        src: 'https://raw.githubusercontent.com/AaronMelhaff/AdventureProgram/master/src/resources/screenshots/using-map.PNG',
+        caption: 'Opening the game map. X is your current location.'
+      },
+    ],
+    desc: 'This is an example of an text adventure game I wrote for one of my Java courses. The topic of the assignment was array traversals, so I made a game about traversing a 2d array and fighting monsters.',
+    repository: 'https://github.com/AaronMelhaff/AdventureProgram',
     tags: ['java']
   },
   {
     title: 'Hunger Games',
-    url: 'hunger-games',
+    id: 'hunger-games',
     date: 'Spring 2017',
-    component: NoMatch,
-    tags: ['java', 'data-structures']
+    desc: 'This was a program that I wrote as my final project for Java 2. I had a bunch of characters that would traverse a node graph. Whenever a character encountered another, they would battle. This would happen until there was only a survivor. At the end, you could look at the log and see all of the battles that occured.',
+    repository: 'https://github.com/AaronMelhaff/HungerGames',
+    tags: ['java', 'data-structures', 'file io']
   },
   {
     title: 'Web Crawler',
-    url: 'web-crawler',
+    id: 'web-crawler',
     date: 'Fall 2017',
-    component: NoMatch,
+    desc: 'I made this webcrawler for my Systems Programming class. It jumps from site to site using anchor tags.',
+    repository: 'https://github.com/AaronMelhaff/WebCrawler',
     tags: ['java']
   },
   {
     title: 'Chat Program',
-    url: 'chat-program',
+    id: 'chat-program',
     date: 'Fall 2017',
-    component: NoMatch,
+    desc: 'Small chat program we wrote in class one day.',
+    repository: 'https://github.com/AaronMelhaff/ChatProgram',
     tags: ['java', 'sockets']
   },
   {
     title: 'Assassin Program',
-    url: 'assassin-program',
+    id: 'assassin-program',
     date: 'Spring 2017',
-    component: NoMatch,
+    desc: 'Program I made for my Java 2 class that demonstrates linked lists.',
+    repository: 'https://github.com/AaronMelhaff/AssassinManager',
     tags: ['java', 'data-structures']
   },
   {
     title: 'Name Registry',
-    url: 'name-registry',
+    id: 'name-registry',
     date: 'Spring 2017',
-    component: NoMatch,
+    desc: 'This is a basic program I wrote to demonstrate file IO with Java',
+    repository: 'https://github.com/AaronMelhaff/NameRegistry',
     tags: ['java', 'file io']
   },
   {
     title: 'Note Taker',
-    url: 'note-taker',
+    id: 'note-taker',
     date: 'Fall 2018',
-    component: NoMatch,
+    desc: 'I wrote this JavaFX program for my design patterns class. It allows users to take different kinds of notes.',
+    repository: 'https://github.com/AaronMelhaff/NoteTaker',
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'Paint Program',
-    url: 'paint-program',
+    id: 'paint-program',
     date: 'Fall 2018',
-    component: NoMatch,
+    desc: 'I wrote this JavaFX program for my design patterns class. It works like MS Paint.',
+    repository: 'https://github.com/AaronMelhaff/PaintProgram',
     tags: ['java', 'design-patterns', 'java-fx']
   },
   {
     title: 'My Portfolio',
-    url: 'bootstrap-portfolio',
+    id: 'bootstrap-portfolio',
     date: 'Spring 2019',
-    component: NoMatch,
+    desc: 'This is my current rendition of my portfolio. I made it using React and Bootstrap.',
+    repository: 'https://github.com/AaronMelhaff/PaintProgram',
     tags: ['javascript', 'react', 'web-development', 'bootstrap', 'front-end', 'sass']
   },
   {
     title: 'Gloomtility',
-    url: 'gloomtility',
+    id: 'gloomtility',
     date: 'Spring 2019',
-    component: NoMatch,
+    desc: 'This was a group project I did with other students.',
     tags: ['javascript', 'java','web-development', 'front-end', 'restful', 'api', 'json', 'postgresql', 'full-stack', 'aws', 'react', 'redux', 'spring', 'spring-boot', 'sass']
   },
   {
     title: 'Schedule-IT',
-    url: 'schedule-it',
+    id: 'schedule-it',
     date: 'Summer 2018',
-    component: NoMatch,
+    desc: 'Scheduling app I made during my school internship.',
     tags: ['javascript', 'jquery', 'web-development', 'front-end', 'sass', 'php', 'api', 'mysql', 'sql', 'full-stack', 'json', 'bootstrap']
   }
 ];
