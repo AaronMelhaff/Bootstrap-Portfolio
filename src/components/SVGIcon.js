ee008d4f25b813a163fb7ed45cf52b60f767035a
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-07-15 11:43:37 -0700

    Fixed stroke width attribute syntax.

--- src/components/SVGIcon.js
+++ src/components/SVGIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SVGIcon = (props) => {
   return (
     <svg className={props.className} viewBox="0 0 24 24">
-      <path stroke-width="0.2" stroke={props.stroke} fill={props.fill} d={props.path} /> 
+      <path strokeWidth="0.2" stroke={props.stroke} fill={props.fill} d={props.path} /> 
     </svg>
   );
 };
